c7c2b49911856278d513a91c8327c7d00b60d476
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-06-09 16:21:28 -0400

    combined patient, admission, lab, vitals dataframes

--- sql_queries/make_dataset/feature_engineering.sql
+++ sql_queries/make_dataset/feature_engineering.sql
@@ -1,3 +1,4 @@
+-- (QUERY NOT USED, TOO RESTRICTIVE) calculate summary measurements for lab table and save to csv
 COPY 
 (SELECT l.patientunitstayid, 
        l.labname, 
@@ -15,6 +16,7 @@ COPY
  GROUP BY l.patientunitstayid, l.labname) 
 TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/lab_summary.csv' (format csv);
 
+-- calculate summary measurements for vital periodic table and write to csv
 COPY
 (SELECT vp.patientunitstayid,
        MIN(vp.sao2) AS min_sao2, MAX(vp.sao2) AS max_sao2, AVG(vp.sao2) AS mean_sao2,
@@ -27,4 +29,23 @@ COPY
  GROUP BY vp.patientunitstayid) 
 TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/vitalperiodic_summary.csv' (format csv);
 
- 
+ -- merge relevant tables of categorical data to encode in python
+ SELECT p.patientUnitStayID, p.gender, p.age, p.ethnicity, p.unitType, p.unitVisitNumber, p.unitDischargeOffset, p.unitDischargeStatus
+   FROM patient p
+
+-- have to use data prior to 24h for labs otherwize too many missing values for positive class
+COPY
+(SELECT l.patientunitstayid, 
+       l.labname, 
+       MIN(l.labresult) AS min_result, 
+       MAX(l.labresult) AS max_result, 
+       MAX(l.labresult)-MIN(labresult) AS delta_result
+  FROM lab l
+ INNER JOIN qualifying_admissions qa
+ ON l.patientunitstayid=qa.patientunitstayid
+ WHERE l.labresultoffset/60<12
+   AND l.labname in ('bedside glucose', 'potassium', 'sodium', 'glucose', 'Hgb', 'Hct', 'chloride', 'creatinine', 'BUN', 'calcium', 
+                   'bicarbonate', 'platelets x 1000', 'WBC x 1000', 'RBC', 'MCV', 'MCHC', 'anion gap', 'RDW', 'MCH', 'FiO2', 'paO2', 
+                   'paCO2', 'pH', 'MPV', 'HCO3', 'magnesium')
+ GROUP BY l.patientunitstayid, l.labname) 
+ TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/lab_summary_prior.csv' (format csv);
