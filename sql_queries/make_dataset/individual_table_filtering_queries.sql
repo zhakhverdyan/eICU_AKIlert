51c1457788705267c7db92f234642deb6efacff6
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-06-08 21:46:17 -0400

    corrected pat_id, label table and pulled diagnosis offset

--- sql_queries/make_dataset/individual_table_filtering_queries.sql
+++ sql_queries/make_dataset/individual_table_filtering_queries.sql
@@ -1,5 +1,5 @@
 COPY 
-(SELECT t1.patientunitstayid, t4.aki_in_diagnosis
+(SELECT t1.patientunitstayid, t4.aki_in_diagnosis, t4.aki_offset
 FROM
 --this query selects patientunitstayids that do not have kidney problems at admission
 (SELECT a.patientunitstayid
@@ -41,12 +41,18 @@ AND LENGTH(p.age)>1
 AND p.unitDischargeOffset/60 > 24) t3
 ON t2.patientunitstayid=t3.patientunitstayid
 INNER JOIN
--- this query labels patientunitstayid as 1/0 for aki development after 24h
-(SELECT d.patientunitstayid,
-CASE WHEN d.concat_diag ILIKE '%acute renal failure%' THEN 1 ELSE 0 END AS aki_in_diagnosis
+-- this query labels patientunitstayid as 1/0 for aki development after 24h as well as pull diagnosis time
+(SELECT d1.patientunitstayid, d2.aki_offset,
+CASE WHEN d1.concat_diag ILIKE '%acute renal failure%' THEN 1 ELSE 0 END AS aki_in_diagnosis
 FROM
 (SELECT patientunitstayid, string_agg(diagnosisstring, ', ') AS concat_diag
 FROM diagnosis
-GROUP BY patientunitstayid) d) t4
+GROUP BY patientunitstayid) d1
+FULL OUTER JOIN
+(SELECT DISTINCT ON (patientunitstayid) patientunitstayid, diagnosisoffset AS aki_offset
+FROM diagnosis
+WHERE diagnosisstring ILIKE '%acute renal failure%'
+ORDER BY 1, 2 ASC) d2
+ON d1.patientunitstayid=d2.patientunitstayid) t4
 ON t3.patientunitstayid=t4.patientunitstayid)
-TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/patid_aki_label.csv' (format csv);
+TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/patid_aki_label_offset.csv' (format csv);
