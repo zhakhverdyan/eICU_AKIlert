3c5717c196997b09f5136e5dee8b029d98f45ffb
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-06-21 12:54:39 -0400

    lab data wrangling worked out

--- sql_queries/make_dataset/qual_pat_aki_timestamp.sql
+++ sql_queries/make_dataset/qual_pat_aki_timestamp.sql
@@ -1,32 +1,24 @@
 -- define qualifying patients >20 years old
 -- no past history of kidney insuficiency, failure, obstruction or neoplasm
 COPY
-(SELECT p.patientunitstayid,
+(SELECT DISTINCT p.patientunitstayid
    FROM patient p
   INNER JOIN pasthistory ph
      ON p.patientunitstayid = ph.patientunitstayid
-    AND ph.pasthistoryvalue NOT ILIKE '%renal failure%'
+  WHERE ph.pasthistoryvalue NOT ILIKE '%renal failure%'
     AND ph.pasthistoryvalue NOT ILIKE '%renal insufficiency%'
     AND CAST(p.age as char) NOT LIKE '1%'
     AND CAST(p.age as char) NOT LIKE '0%'
     AND LENGTH(p.age)>1)
-TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate_files/qualifying_admissions_final.csv' (format csv);
+TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/qualifying_admissions_final.csv' (format csv);
 
 DROP TABLE IF EXISTS qualifying_admissions CASCADE;
 CREATE TABLE qualifying_admissions
 (
-  patientunitstayid BIGINT NOT NULL,
-  age VARCHAR(10),
-  gender VARCHAR(25),
-  ethnicity VARCHAR(50),
-  unitDischargeOffset INT,
-  admitdxpath VARCHAR(500) NOT NULL,
-  pasthistoryvalue VARCHAR(100)
-  developed_aki INT NOT NULL,
-  aki_diagnosis INT
+  patientunitstayid BIGINT NOT NULL
 ) ;
 -- load the data
- \copy qualifying_admissions FROM '~/Insight/eICU_dbmake/eicu-code/build-db/postgres/intermediate_files/qualifying_admissions_final.csv' DELIMITER ',' CSV HEADER NULL ''
+ \copy qualifying_admissions FROM '~/Insight/eICU_AKIlert/data/intermediate/qualifying_admissions_final.csv' DELIMITER ',' CSV HEADER NULL ''
 
 
 -- get all creatinine values for qualifying pateints to detect earliest aki onset
@@ -39,9 +31,10 @@ COPY
      ON l.patientunitstayid=qa.patientunitstayid
   INNER JOIN patient p
      ON p.patientunitstayid=l.patientunitstayid
-  WHERE l.labname='creatinine'
+  WHERE l.labname='creatinine')
 TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/creatinine_lev_all.csv' (format csv);
 
+/*
 -- get all urine output measurements for qualifying patients to detect earliest aki onset
 COPY
 (SELECT DISTINCT i.patientunitstayid,
@@ -53,3 +46,4 @@ COPY
      ON i.patientunitstayid=qa.patientunitstayid
  WHERE i.celllabel='Urine' OR i.celllabel='Bodyweight (kg)'
 TO '/Users/zhannahakhverdyan/Insight/eICU_AKIlert/data/intermediate/urine_out_all_3d.csv' (format csv);
+*/
---
+++ src/aki_label.py
@@ -0,0 +1,50 @@
+import math
+import numpy as np
+import pandas as pd
+ 
+def creat_label_timeoffset(df):
+    """Calculate the difference between consecutive creatinine measurements and identify if abnormal according to KDIGO standards"""
+    # replicate labresult column and push 1 down to calculate the difference between current and previous measurement
+    df['prev_labresult'] = df.groupby(['patientunitstayid'])['labresult'].shift(1)
+    df['delta'] = df['labresult'] - df['prev_labresult']
+
+    # calculate a rolling sum of creatinine differences over 48 h rolling window
+    df['labresultoffset'] = pd.to_timedelta(df['labresultoffset'], unit='minute')
+    df.set_index('labresultoffset', inplace=True)
+    df['creat_2day_cum'] = df.groupby('patientunitstayid')['delta'].apply(lambda x: x.rolling('2d').sum()) 
+
+    # calculate a cumulative fold increase from baseline over 7 day rolling window
+    df['creat_fold_change'] = df['labresult']/df['prev_labresult']
+    df['creat_7day_inc'] = df.groupby('patientunitstayid')['creat_fold_change'].apply(lambda x: x.rolling('7d').apply(np.prod))
+
+    # label abnormal measurement rows
+    df.loc[df['creat_2day_cum'] >= 0.3, 'AKI_reached_2d'] = 1
+    df.loc[df['creat_7day_inc'] >= 1.5, 'AKI_reached_7d'] = 1
+    df.fillna(0, inplace=True)
+
+    # create a new dataframe to collect only the first instances of the positive class
+    df.reset_index(inplace=True)
+    first_2d = df[df['AKI_reached_2d']==1].groupby('patientunitstayid')['labresultoffset'].first()
+    first_2d = first_2d/pd.Timedelta('1 minute')
+    first_7d = df[df['AKI_reached_7d']==1].groupby('patientunitstayid')['labresultoffset'].first()
+    first_7d = first_7d/pd.Timedelta('1 minute')
+    first_occur_comb = pd.concat([first_2d, first_7d], axis=1, ignore_index=False)
+    first_occur_comb.fillna(0, inplace=True)
+    first_occur_comb.columns = ['2d_first', '7d_first']
+
+    # identify the lesser non-zero time offset
+    earlier_time_list = []
+    for index, row in first_occur_comb.iterrows():
+        earlier_time = min(row)
+        if earlier_time==0:
+            row_list = row.values.tolist()
+            row_list.remove(earlier_time)
+            earlier_time_list.append(row_list[0])
+        else:
+            earlier_time_list.append(earlier_time)
+
+    earlier_time_df = pd.DataFrame({'patientunitstayid':first_occur_comb.index, 'earlier_time': earlier_time_list})
+    earlier_time_df.set_index('patientunitstayid', inplace=True)
+    first_occur_comb = pd.concat([first_occur_comb, earlier_time_df], axis=1, ignore_index=False)
+
+    return df, first_occur_comb
